7119e89cd4530a1f6b40e56651975b74aed00060
srhtylmz19 <srhtylmz19@gmail.com>
2020-03-13 23:41:06 +0300

    storing data to sqlite database

--- sqlite-flask/code/item.py
+++ sqlite-flask/code/item.py
@@ -9,28 +9,44 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
+
+        query2 = "SELECT * FROM items"
+        result2 = cursor.execute(query2)
+        row2 = result2.fetchall()
+        for itm in row2:
+            print(itm)
 
         query = "SELECT * FROM items WHERE name=?"
         result = cursor.execute(query, (name,))
         row = result.fetchone()
-
         connection.close()
 
         if row:
             return {'item': {'name': row[0], 'price': row[1]}}
-        return {'message': 'Item not found'}, 404
 
-    @jwt_required()
     def post(self, name):
-
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return {'message': "Item with name '{}' already exist".format(name)}
 
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
         return item, 201
 
     @jwt_required()
